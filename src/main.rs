32c4caadd51a3dc3db4a8b2621eec740c9a40a7b
Dennis Chan <dennisming2012@gmail.com>
2024-08-31 18:44:31 +0800

    feat: write json to home directory

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use dirs;
 use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,6 +10,15 @@ fn main() {
     if args.len() > 1 {
         let input = &args[1];
         println!("Input argument: {}", input);
+
+        let home_dir = dirs::home_dir().expect("Could not find home directory");
+        let mut file_path = PathBuf::from(home_dir);
+        file_path.push(".rustash.json");
+
+        let mut file = File::create(file_path).expect("Could not create file");
+
+        file.write_all(input.as_bytes())
+            .expect("Could not write to file");
     } else {
         println!("No input argument provided");
     }
