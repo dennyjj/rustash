ab5a9771a44c0324eabf39cc53719b05c631b875
Dennis Chan <dennisming2012@gmail.com>
2024-09-13 21:02:02 +0800

    refactor: config

--- src/config.rs
+++ src/config.rs
@@ -6,34 +6,41 @@ pub struct Config {
     pub msg: Option<String>,
 }
 
+fn get_command(args: &[String]) -> Result<Command, Box<dyn Error>> {
+    if args.len() < 2 {
+        return Err("not enough arguments".into());
+    }
+
+    match args[1].as_str() {
+        "add" => Ok(Command::Add),
+        "list" => Ok(Command::List),
+        "show" => {
+            if args.len() > 2 {
+                let index = args[2].parse::<usize>().map_err(|_| "invalid index")?;
+                Ok(Command::Show(Some(index)))
+            } else {
+                Ok(Command::Show(None))
+            }
+        }
+        "remove" => {
+            if args.len() > 2 {
+                let index = args[2].parse::<usize>().map_err(|_| "invalid index")?;
+                Ok(Command::Remove(index))
+            } else {
+                Err("provide an index for remove command".into())
+            }
+        }
+        "clear" => Ok(Command::Clear),
+        _ => Err("invalid command, accepted commands: add, list, clear, show".into()),
+    }
+}
+
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, Box<dyn Error>> {
         if args.len() < 2 {
             return Err("not enough arguments".into());
         }
-
-        let command = match args[1].as_str() {
-            "add" => Command::Add,
-            "list" => Command::List,
-            "show" => {
-                if args.len() > 2 {
-                    let index = args[2].parse::<usize>().map_err(|_| "invalid index")?;
-                    Command::Show(Some(index))
-                } else {
-                    Command::Show(None)
-                }
-            }
-            "remove" => {
-                if args.len() > 2 {
-                    let index = args[2].parse::<usize>().map_err(|_| "invalid index")?;
-                    Command::Remove(index)
-                } else {
-                    return Err("provide an index for remove command".into());
-                }
-            }
-            "clear" => Command::Clear,
-            _ => return Err("invalid command, accepted commands: add, list, clear, show".into()),
-        };
+        let command = get_command(args);
 
         if let Command::Add = command {
             if args.len() < 3 {
